c0aded570d189d38a76ba67ec7a0ac7e3d499fd9
Sean Cheung <sean.816.sean@gmail.com>
2024-06-03 14:33:36 +0800

    Uses lombok annotation
    Signed-off-by: Sean Cheung <sean.816.sean@gmail.com>

--- src/main/java/dev/redcoke/mcserverping/MCServerPingResponse.java
+++ src/main/java/dev/redcoke/mcserverping/MCServerPingResponse.java
@@ -4,27 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.jetbrains.annotations.Nullable;
 
+@Data
+@AllArgsConstructor
 public final class MCServerPingResponse {
   private final int ping;
   private final String version;
   private final int protocol;
-  private final int maxPlayers;
-  private final int onlinePlayers;
+  @Nullable private final Integer maxPlayers;
+  @Nullable private final Integer onlinePlayers;
   private final String motd;
   private final JsonArray descriptionExtras;
   private final String serverIcon;
-
-  public MCServerPingResponse(int ping, String name, int protocol, Integer playerMax, Integer playerOnline, String motd, JsonArray descriptionExtras, String serverIcon) {
-    this.ping = ping;
-    this.version = name;
-    this.protocol = protocol;
-    this.maxPlayers = playerMax;
-    this.onlinePlayers = playerOnline;
-    this.motd = motd;
-    this.descriptionExtras = descriptionExtras;
-    this.serverIcon = serverIcon;
-  }
 
   public static MCServerPingResponse serverPingFromJsonObj(JsonObject jsonObj) {
     int serverPing = jsonObj.get("ping").getAsInt();
@@ -42,38 +36,6 @@ public final class MCServerPingResponse {
     return new MCServerPingResponse(
             serverPing, versionName, serverProtocol, playerMax, playerOnline, serverMOTD, serverDescriptionExtra, favIcon
     );
-  }
-
-  public int getPing() {
-    return ping;
-  }
-
-  public String getName() {
-    return version;
-  }
-
-  public int getProtocol() {
-    return protocol;
-  }
-
-  public Integer getPlayerMax() {
-    return maxPlayers;
-  }
-
-  public Integer getPlayerOnline() {
-    return onlinePlayers;
-  }
-
-  public String getMotd() {
-    return motd;
-  }
-
-  public JsonArray getDescriptionExtras() {
-    return descriptionExtras;
-  }
-
-  public String getServerIcon() {
-    return serverIcon;
   }
 
   public String getAsJsonString() {
